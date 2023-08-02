7bee7cd5d473f3c8a898034b70747ce3789b6eb3
Guilherme Gomes <guilhermegg018@gmail.com>
2023-08-02 20:58:24 -0300

    implementando remocao logica do curso

--- src/main/java/com/guilherme/crudspring/model/Course.java
+++ src/main/java/com/guilherme/crudspring/model/Course.java
@@ -1,5 +1,7 @@
 package com.guilherme.crudspring.model;
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -16,6 +18,8 @@ import lombok.Data;
 
 @Data
 @Entity
+@SQLDelete(sql = "UPDATE Course SET status = 'Inactive' WHERE id = ?")
+@Where(clause = "status = 'Active'")
 public class Course {
 
     @Id
@@ -35,4 +39,9 @@ public class Course {
     @Column(length = 10, nullable = false)
     private String category;
 
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Active|Inactive")
+    @Column(length = 10, nullable = false)
+    private String status = "Active";
 }

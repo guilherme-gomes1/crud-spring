808cc5bef50f62c07e85235a770bfe8f4ae290f7
Guilherme Gomes <guilhermegg018@gmail.com>
2023-08-05 19:32:49 -0300

    criando a coluna de aula e relacionando ela com a coluna de curso

--- src/main/java/com/guilherme/crudspring/CrudSpringApplication.java
+++ src/main/java/com/guilherme/crudspring/CrudSpringApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import com.guilherme.crudspring.enums.Category;
 import com.guilherme.crudspring.enums.Status;
 import com.guilherme.crudspring.model.Course;
+import com.guilherme.crudspring.model.Lesson;
 import com.guilherme.crudspring.repository.CourseRepository;
 
 @SpringBootApplication
@@ -26,6 +27,12 @@ public class CrudSpringApplication {
 			c.setName("Angular");
 			c.setCategory(Category.FRONT_END);
 			c.setStatus(Status.ACTIVE);
+
+			Lesson l = new Lesson();
+			l.setName("null");
+			l.setYoutubeUrl("null");
+			l.setCourse(c);
+			c.getLessons().add(l);
 
 			courseRepository.save(c);
 		};
